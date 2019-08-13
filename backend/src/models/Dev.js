a0c8dbf1fed3cb871c2fd6701e30145437851f37
AnderLyws <ander.lyws@gmail.com>
2019-08-13 09:51:54 -0300

    Add Backend

---
+++ backend/src/models/Dev.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const DevSchema = new Schema({
+    user: {
+        type: String,
+        required: true,
+    },
+    bio: String,
+    avatar: {
+        type: String,
+        required: true,
+    },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev',
+    }],
+    dislikes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev',
+    }],
+}, {
+    timestamps: true,
+
+});
+
+module.exports = model('Dev', DevSchema);
